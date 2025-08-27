94d8a07732972cabfa7bb363b96680fa3811672d
Monal <mob.jupical@gmail.com>
2025-08-27 19:48:10 +0530

    Till Prompt and Message History

---
+++ Prompt/prompt_ui.py
@@ -0,0 +1,39 @@
+from langchain_huggingface import ChatHuggingFace , HuggingFaceEndpoint
+from dotenv import load_dotenv
+import streamlit as st
+from langchain_core.prompts import PromptTemplate,load_prompt
+
+load_dotenv()
+
+llm = HuggingFaceEndpoint(
+    repo_id= "deepseek-ai/DeepSeek-V3.1",
+    task= "text-generation"
+)
+
+model = ChatHuggingFace(llm = llm)
+
+
+st.header('Research Tool')
+
+paper_input = st.selectbox( "Select Research Paper Name", ["Attention Is All You Need", "BERT: Pre-training of Deep Bidirectional Transformers", "GPT-3: Language Models are Few-Shot Learners", "Diffusion Models Beat GANs on Image Synthesis"] )
+
+style_input = st.selectbox( "Select Explanation Style", ["Beginner-Friendly", "Technical", "Code-Oriented", "Mathematical"] ) 
+
+length_input = st.selectbox( "Select Explanation Length", ["Short (1-2 paragraphs)", "Medium (3-5 paragraphs)", "Long (detailed explanation)"] )
+
+
+# Template
+
+template = load_prompt('Prompt/template.json')
+
+# Fill the placeholder
+prompt = template.invoke({
+    'paper_input' : paper_input,
+    'style_input' : style_input,
+    'length_input' : length_input
+})
+
+if st.button('Summarize'):
+    result = model.invoke(prompt)
+    st.write(result.content)
+
